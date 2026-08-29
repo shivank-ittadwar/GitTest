f07c0da4d82547d9e0ab415a83d84761b1521ea9
Shivank Ittadwar <shivankittadwar00@gmail.com>
2026-08-29 06:07:22 -0700

    Add shivank program

---
+++ shivank.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+
+    cout<<"enter the number : ";
+    cin>>a;
+
+    cout<<"enter the number :";
+    cin>>b;
+
+    cout<<a+b;
+}
